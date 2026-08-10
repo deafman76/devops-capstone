375043e4e4e61030237fc19a66fddd5d51afd2e9
varunbknd7 <100609984+varunbknd7@users.noreply.github.com>
2026-08-11 02:47:52 +0530

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,15 +1,3 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "security_group_id" {
-  value = aws_security_group.web.id
-}
-
 output "instance_id" {
   value = aws_instance.web.id
 }
@@ -18,14 +6,6 @@ output "instance_public_ip" {
   value = aws_instance.web.public_ip
 }
 
-output "instance_private_ip" {
-  value = aws_instance.web.private_ip
-}
-
-output "application_url" {
-  value = "http://${aws_instance.web.public_ip}"
-}
-
-output "ssh_command" {
-  value = "ssh -i <your-key.pem> ec2-user@${aws_instance.web.public_ip}"
+output "s3_bucket_name" {
+  value = aws_s3_bucket.app.bucket
 }
